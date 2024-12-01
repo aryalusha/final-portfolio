fa6fb876c253bbf99eee868c3280f3ac347f48a1
aryalusha <aryalanee14@gmail.com>
2024-12-01 21:41:39 +0545

    almost complete

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ease = "power4.inOut";
+
+  document.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", (event) =>{
+      event.preventDefault();
+      const href = link.getAttribute("href");
+
+    if (href && (href.includes('behance.net') || href.includes('dribbble.com'))) {
+      event.preventDefault();
+      animateTransition().then(() => {
+        const newTab = window.open(href, '_blank');
+        if (newTab) {
+          newTab.focus();
+          localStorage.setItem('newTabOpened', 'true');
+          window.location.reload();
+        }
+      });
+    } else {
+      if (href && !href.startsWith("#") && href !== window.location.pathname) {
+        event.preventDefault();
+        animateTransition().then(() => {
+          window.location.href = href; 
+        });
+      }
+    }
+  });
+});
+  
+  revealTransition().then(() => {
+    gsap.set(".block",  {visiblity: "hidden"});
+  });
+
+  function revealTransition() {
+    return new Promise((resolve) => {
+      gsap.set(".block", {scaleY: 1});
+      gsap.to(".block", {
+        scaleY: 0,
+        duration: 1, 
+        stagger: {
+          each: 0.1,
+          from: "start", 
+          grid: "auto", 
+          axis: "x",
+        },
+        ease: ease,
+        onComplete: resolve,
+      });
+    });
+  }
+
+  function animateTransition(){
+    return new Promise((resolve) => {
+      gsap.set(".block", {visiblity: "visible", scaleY: 0});
+      gsap.to(".block", {
+        scaleY: 1, 
+        duration: 1, 
+        stagger: {
+          each: 0.1, 
+          from: "start", 
+          grid: [2, 5], 
+          axis: "x", 
+        },
+
+        ease: ease, 
+        onComplete: resolve,
+      });
+    });
+  }
+});
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -155,7 +230,6 @@ gsap.fromTo(
     },
   }
 );
-
 
 
 const nav = document.querySelector(".mainHead");
